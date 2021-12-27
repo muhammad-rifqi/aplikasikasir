f8b54d10cd916da5c0cd6e5abd5f0a91e72487a9
rizskydarmawann <70370415+rizskydarmawann@users.noreply.github.com>
2021-12-27 12:50:05 +0700

    Update aplikasikasir.sql
    db baru

--- aplikasikasir.sql
+++ aplikasikasir.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 25, 2021 at 06:35 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 7.3.28
+-- Generation Time: Dec 27, 2021 at 06:48 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.3.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -39,7 +39,34 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `username`, `email`, `password`) VALUES
-(1, 'admin', 'admin@localhost', '21232f297a57a5a743894a0e4a801fc3');
+(1, 'Rizsky Darmawan', 'admin@localhosta.com', '21232f297a57a5a743894a0e4a801fc3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `warung`
+--
+
+CREATE TABLE `warung` (
+  `id` int(11) NOT NULL,
+  `nama_warung` varchar(100) NOT NULL,
+  `pajak_perhari` int(11) NOT NULL,
+  `total_terjual` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `keterangan` text NOT NULL,
+  `foto` varchar(225) NOT NULL,
+  `alamat` text NOT NULL,
+  `kontak` varchar(100) NOT NULL,
+  `modify` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `warung`
+--
+
+INSERT INTO `warung` (`id`, `nama_warung`, `pajak_perhari`, `total_terjual`, `tanggal`, `keterangan`, `foto`, `alamat`, `kontak`, `modify`) VALUES
+(1, 'Gajah Tunggal', 10000, 2, '2021-12-27', 'fknekfnkcnkscn', '', 'nwfkwnfwkfnwkfnwk', '0909smnsknck', '2021-12-27'),
+(3, 'Gajah Tunggal', 10000, 2, '2021-12-27', 'fknekfnkcnkscn', '', 'nwfkwnfwkfnwkfnwk', '0909smnsknck', '2021-12-27');
 
 --
 -- Indexes for dumped tables
@@ -52,6 +79,12 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`);
 
 --
+-- Indexes for table `warung`
+--
+ALTER TABLE `warung`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -60,6 +93,12 @@ ALTER TABLE `user`
 --
 ALTER TABLE `user`
   MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `warung`
+--
+ALTER TABLE `warung`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
