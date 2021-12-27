c45a7898605c98c65ae817bc6c8cf36b43076335
rizskydarmawann <70370415+rizskydarmawann@users.noreply.github.com>
2021-12-27 09:27:56 +0700

    Update warung.php
    d

--- application/views/admin/warung.php
+++ application/views/admin/warung.php
@@ -3,15 +3,18 @@
           <div class="section-header">
             <h1><?= $title; ?></h1>
           </div>
-
           <div class="section-body">
-           
-
             <div class="row">
               <div class="col-12 col-md-6 col-lg-12">
                 <div class="card">
-                  <div class="card-header">
-                    <h4>Simple Table</h4>
+                  <div class="card-header d-flex">
+                    <h4>List Warung</h4>
+                    <div class="ml-auto pr-2">
+                        <a href="" class="btn btn-primary">
+                            <i class="fas fa-plus"></i> Tambah
+                        </a>
+                    </div>
+                   
                   </div>
                   <div class="card-body">
                     <div class="table-responsive">
