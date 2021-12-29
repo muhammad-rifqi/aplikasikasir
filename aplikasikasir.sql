6165c3667fec95891cfc6ce84f54d427797807bd
unknown <muhammad45rifki@gmail.com>
2021-12-29 18:44:33 +0700

    update databases

--- aplikasikasir.sql
+++ aplikasikasir.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 27, 2021 at 06:48 AM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 7.3.31
+-- Generation Time: Dec 29, 2021 at 12:43 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 7.3.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,57 @@ SET time_zone = "+00:00";
 --
 -- Database: `aplikasikasir`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang_keluar`
+--
+
+CREATE TABLE `barang_keluar` (
+  `id_barang_keluar` int(11) NOT NULL,
+  `id_warung` int(11) NOT NULL,
+  `nama_produk` varchar(255) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `keterangan` text NOT NULL,
+  `foto` varchar(255) NOT NULL,
+  `tangga_update` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang_masuk`
+--
+
+CREATE TABLE `barang_masuk` (
+  `id_barang_masuk` int(11) NOT NULL,
+  `id_warung` int(11) NOT NULL,
+  `nama_produk` varchar(255) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `keterangan` text NOT NULL,
+  `foto` varchar(255) NOT NULL,
+  `tangga_update` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `id_produk` int(11) NOT NULL,
+  `id_warung` int(11) NOT NULL,
+  `nama_produk` varchar(255) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `keterangan` text NOT NULL,
+  `foto` varchar(255) NOT NULL,
+  `tangga_update` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -39,7 +90,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `username`, `email`, `password`) VALUES
-(1, 'Rizsky Darmawan', 'admin@localhosta.com', '21232f297a57a5a743894a0e4a801fc3');
+(1, 'Rizsky Darmawan', 'admin@localhost.com', '21232f297a57a5a743894a0e4a801fc3');
 
 -- --------------------------------------------------------
 
@@ -65,12 +116,31 @@ CREATE TABLE `warung` (
 --
 
 INSERT INTO `warung` (`id`, `nama_warung`, `pajak_perhari`, `total_terjual`, `tanggal`, `keterangan`, `foto`, `alamat`, `kontak`, `modify`) VALUES
-(1, 'Gajah Tunggal', 10000, 2, '2021-12-27', 'fknekfnkcnkscn', '', 'nwfkwnfwkfnwkfnwk', '0909smnsknck', '2021-12-27'),
-(3, 'Gajah Tunggal', 10000, 2, '2021-12-27', 'fknekfnkcnkscn', '', 'nwfkwnfwkfnwkfnwk', '0909smnsknck', '2021-12-27');
+(1, 'Gajah Tunggal', 10000, 2, '2021-12-27', 'fknekfnkcnkscn', 'http://localhost/aplikasikasir/assets/upload/warung/profil-andre.jpg', 'nwfkwnfwkfnwkfnwk', '0909smnsknck', '2021-12-28'),
+(3, 'Gajah Tunggal', 10000, 2, '2021-12-27', 'fknekfnkcnkscn', 'http://localhost/aplikasikasir/assets/upload/warung/profil-usman.jpeg', 'nwfkwnfwkfnwkfnwk', '0909smnsknck', '2021-12-29'),
+(5, 'warung serba guna', 87000000, 1209090, '2021-01-01', 'keterangan', 'http://localhost/aplikasikasir/assets/upload/warung/profil-naufal.jpg', 'ciputat', 'kontak', '2021-12-29');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `barang_keluar`
+--
+ALTER TABLE `barang_keluar`
+  ADD PRIMARY KEY (`id_barang_keluar`);
+
+--
+-- Indexes for table `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  ADD PRIMARY KEY (`id_barang_masuk`);
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id_produk`);
 
 --
 -- Indexes for table `user`
@@ -89,6 +159,24 @@ ALTER TABLE `warung`
 --
 
 --
+-- AUTO_INCREMENT for table `barang_keluar`
+--
+ALTER TABLE `barang_keluar`
+  MODIFY `id_barang_keluar` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  MODIFY `id_barang_masuk` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
@@ -98,7 +186,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `warung`
 --
 ALTER TABLE `warung`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
