18e12942d075331b3df26c5924751c018eb756cc
rizskydarmawann <70370415+rizskydarmawann@users.noreply.github.com>
2021-12-30 10:49:47 +0700

    kn
    kn

--- application/views/tamplates/auth_header.php
+++ application/views/tamplates/auth_header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <title><?= $title; ?></title>
+    <link rel="icon" type="image/x-icon" href="<?= base_url('assets/'); ?>img/Lambang_Kabupaten_Takalar.png">
     <!-- Custom fonts for this template-->
     <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendor/bootstrap/css/bootstrap.min.css" type="text/css">
     <link href="<?= base_url('assets/'); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
