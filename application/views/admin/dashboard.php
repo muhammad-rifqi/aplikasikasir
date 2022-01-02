1c85684b50399c8403af936e110c8aa6937403d5
rizskydarmawann <70370415+rizskydarmawann@users.noreply.github.com>
2022-01-02 20:06:04 +0700

    j
    n

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -8,7 +8,7 @@
             <div class="col-lg-3 col-md-6 col-sm-6 col-12">
               <div class="card card-statistic-1">
                 <div class="card-icon bg-primary">
-                  <i class="far fa-user"></i>
+                <i class="fas fa-store"></i></i>
                 </div>
                 <div class="card-wrap">
                   <div class="card-header">
